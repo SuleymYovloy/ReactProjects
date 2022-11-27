13ec309026fefb5bd3ffe696febfe4b140ead61d
Suleym <suleymevlo81@gmail.com>
2022-11-27 14:32:36 +0300

    Модальное окно

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React from 'react';
 import './index.scss';
+// import Counter from "./components/Counter";
+import Modal from './components/Modal';
 
 function App() {
+
+  const [open, setOpen] = React.useState(false);
+
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
+      {/* <Counter/> */}
+      <Modal open={open} setOpen={setOpen}>
+        <img  src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt="Картинка"/>
+        <h2>Модальное окно</h2>
+      </Modal>
+      
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Counter/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Counter = () => {
+  const [count, setCount] = React.useState(0)
+
+  // let count = 0;
+
+  const onClickPlus = () => {
+    setCount(count + 1);
+  }
+
+  const onClickMinus = () => {
+    if (count === 0 ) {
+      return false;
+    } 
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+      <h2>Счетчик:</h2>
+      <h1>{count}</h1>
+      <button type="button" disabled={count > 0 ? false : true} onClick={onClickMinus} className="minus">- Минус</button>
+      <button onClick={onClickPlus} className="plus">Плюс +</button>
+    </div>
+  );
+}
+
+export default Counter;
