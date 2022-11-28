9d6930a89c6b8ddf66cf507b76ffd5f9a33a57e7
Suleym <suleymevlo81@gmail.com>
2022-11-28 13:28:08 +0300

    Написал опросник Quiz

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './index.scss';
 // import Counter from "./components/Counter";
-import Modal from './components/Modal';
+// import Modal from './components/Modal';
+import Quiz from './components/Quiz';
 
 function App() {
-
-  const [open, setOpen] = React.useState(false);
+  
+  // const [open, setOpen] = React.useState(false); //Modal
 
   return (
     <div className="App">
       {/* <Counter/> */}
-      <Modal open={open} setOpen={setOpen}>
+      {/* <Modal open={open} setOpen={setOpen}>
         <img  src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt="Картинка"/>
         <h2>Модальное окно</h2>
-      </Modal>
+      </Modal> */}
+      <Quiz/>
       
     </div>
   )
