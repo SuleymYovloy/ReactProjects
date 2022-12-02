edac8b71aa12cc72550f8f0adb7a4ed68e2108ec
Suleym <suleymevlo81@gmail.com>
2022-12-02 23:08:29 +0300

    Приложения, для приглашения пользователей

--- src/App.js
+++ src/App.js
@@ -1,37 +1,88 @@
-import React from 'react';
-import './index.scss';
+import React from "react";
+import "./index.scss";
 import Counter from "./components/Counter";
-import Modal from './components/Modal';
-import Quiz from './components/Quiz';
-import { Success } from './components/Users/Success';
-import { Users } from './components/Users';
-import {Routes, Route} from "react-router-dom"
-// Тут список пользователей: https://reqres.in/api/users
+import Modal from "./components/Modal";
+import Quiz from "./components/Quiz";
+import { Success } from "./components/Users/Success";
+import { Users } from "./components/Users";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  
   const [open, setOpen] = React.useState(false); //Modal
+
+  const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
+  const [success, setSuccess] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState('');
+  console.log(success)
+
+  React.useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка получения пользователей");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value)
+  }
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id !== id));
+    } else {
+      setInvites(prev => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSuccess(true)
+  }
 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/counter" element={<Counter />}/>
-        <Route path="/modal" element={<Modal open={open} setOpen={setOpen}>
-        <img  src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt="Картинка"/>
-        <h2>Модальное окно</h2>
-        </Modal>}/>
-        <Route path="/quiz" element={<Quiz />}/>
-        <Route path="users/" element={< Users/>}/>
-        <Route path='users/success' element={<Success/>}/>
-      </Routes>
-      {/* <Counter/> */}
       
+      <Routes>
+        <Route path="/counter" element={<Counter />} />
+        <Route
+          path="/modal"
+          element={
+            <Modal open={open} setOpen={setOpen}>
+              <img
+                src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+                alt="Картинка"
+              />
+              <h2>Модальное окно</h2>
+            </Modal>
+            
+          }
+        />
+        {/* {
+          success ? <Route path="/success" element={<Success />} /> : <Route path="/users" element={<Users items={users} isLoading={isLoading} searchValue={searchValue} onChangeSearchValue={onChangeSearchValue} invites={invites} onClickInvite={onClickInvite} onClickSendInvites={onClickSendInvites}/>} />
+        } */}
+        <Route path="/quiz" element={<Quiz />} />
+        
+        
+      </Routes>
+      {
+        success ? <Success count={invites.length}/> : <Users items={users} isLoading={isLoading} searchValue={searchValue} onChangeSearchValue={onChangeSearchValue} invites={invites} onClickInvite={onClickInvite} onClickSendInvites={onClickSendInvites}/>
+      }
+      {/* <Counter/> */}
+
       {/* <Quiz/> */}
       {/* <Users/> */}
       {/* <Success/> */}
-      
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -3,7 +3,10 @@ import { Skeleton } from "./Skeleton";
 import { User } from "./User";
 import "./index.scss";
 
-export const Users = ({ items, isLoading }) => {
+// Тут список пользователей: https://reqres.in/api/users
+
+export const Users = ({ items, isLoading, searchValue, onChangeSearchValue, onClickInvite, invites, onClickSendInvites }) => {
+
   return (
     <div className="users-wrapper">
       <div className="users">
@@ -11,7 +14,7 @@ export const Users = ({ items, isLoading }) => {
           <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
           </svg>
-          <input type="text" placeholder="Найти пользователя..." />
+          <input value={searchValue} onChange={onChangeSearchValue} type="text" placeholder="Найти пользователя..." />
         </div>
         {isLoading ? (
           <div className="skeleton-list">
@@ -21,10 +24,23 @@ export const Users = ({ items, isLoading }) => {
           </div>
         ) : (
           <ul className="users-list">
-            <User />
+            {
+              items.filter((item) => {
+                const fullName = (item.first_name + item.last_name).toLowerCase();
+                
+                return fullName.includes(searchValue.toLowerCase()) || item.email.toLowerCase().includes(searchValue.toLowerCase())
+
+              }).map((item) => {
+                return <User onClickInvite={onClickInvite} isInvited={invites.includes(item.id)} key={item.id} data={item}  />;
+              })
+            }
           </ul>
         )}
-        <button className="send-invite-btn">Отправить приглашение</button>
+        {
+          invites.length > 0 && (
+            <button onClick={onClickSendInvites} className="send-invite-btn">Отправить приглашение</button>
+          )
+        }
       </div>
     </div>
   );
