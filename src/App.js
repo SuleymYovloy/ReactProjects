b5981eb214de71e235628c2fc7f86f8c2b3c2440
Suleym <suleymevlo81@gmail.com>
2023-06-17 23:06:39 +0300

    Переписал счетчик на Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import React from "react";
-import "./index.scss";
-import Counter from "./components/Counter";
-import Modal from "./components/Modal";
-import Quiz from "./components/Quiz";
-import Convertor from "./components/Convertor";
-import CollectionApp from "./components/Collection";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import './index.scss';
+import Counter from './components/Counter';
+import Modal from './components/Modal';
+import Quiz from './components/Quiz';
+import Convertor from './components/Convertor';
+import CollectionApp from './components/Collection';
+import { Routes, Route } from 'react-router-dom';
 import { Users } from './components/Users/index';
-import Tabs from "./components/Tabs/Tabs";
+import Tabs from './components/Tabs/Tabs';
 
 function App() {
-
   return (
     <div className="App">
-      <Tabs/>
+      <Tabs />
       <Routes>
         <Route path="/counter" element={<Counter />} />
 
         <Route
           path="/modal"
           element={
-            <Modal >
+            <Modal>
               <img
                 src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
                 alt="Картинка"
               />
               <h2>Модальное окно</h2>
             </Modal>
-            
           }
         />
 
@@ -36,10 +34,9 @@ function App() {
         <Route path="/convertor" element={<Convertor />} />
 
         <Route path="/collections" element={<CollectionApp />} />
-        
+
         <Route path="/collections" element={<CollectionApp />} />
         <Route path="/users" element={<Users />} />
-        
       </Routes>
     </div>
   );
--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,32 +1,31 @@
-import React from "react";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './redux/slices/filterSlice';
 import './index.scss';
 
 const Counter = () => {
-  const [count, setCount] = React.useState(0)
-
-  // let count = 0;
-
-  const onClickPlus = () => {
-    setCount(count + 1);
-  }
-
-  const onClickMinus = () => {
-    if (count === 0 ) {
-      return false;
-    } 
-    setCount(count - 1);
-  }
+  // const [count, setCount] = React.useState(0);
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <div className="Counter">
       <div>
         <h2>Счетчик:</h2>
         <h1>{count}</h1>
-        <button type="button" disabled={count > 0 ? false : true} onClick={onClickMinus} className="minus">- Минус</button>
-        <button onClick={onClickPlus} className="plus">Плюс +</button>
+        <button
+          className="minus"
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button className="plus" aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default Counter;
